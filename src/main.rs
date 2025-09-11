712a3b2ec09258a7a437d552094356f9c54cd863
Chase <crebsto1@students.towson.edu>
2025-09-10 20:21:27 -0400

    challenge 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 fn main() {
-    let a = 13;
-    let b = 2.3;
-    let c: f32 = 120.0;
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32;
+    let mut min: i32;
+    let mut mean: f64;
 
     /* YOUR CODE GOES HERE */
+max = numbers[0];
+min = numbers[0];
+mean = 0.0;
 
-let average = (a as f64 + b as f64 + c as f64) / 3.0;
+    for &num in numbers.iter() {
+        if num > max {
+            max = num;
+        }
+        else if num < min {
+            min = num;
+        }
+        mean += num as f64;
+    }
+mean /= numbers.len() as f64;
 
-    assert_eq!(average, 45.1);
-    println!("Test passed!");
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Tests passed!");
 }
