9995355961ac55b4a1ce35ba578518b60557f024
Chase <crebsto1@students.towson.edu>
2025-09-10 20:04:08 -0400

    challenge

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let x = 5;
-    println!("x is {}", x);
+    let a = 13;
+    let b = 2.3;
+    let c: f32 = 120.0;
+
+    /* YOUR CODE GOES HERE */
+
+let average = (a as f64 + b as f64 + c as f64) / 3.0;
+
+    assert_eq!(average, 45.1);
+    println!("Test passed!");
 }
