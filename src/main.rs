fa9f627948b3a915c4a3c09c517c0da1bb5f8e8f
Chase <crebsto1@students.towson.edu>
2025-09-21 22:56:10 -0400

    lab4#2 solution

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use std::env;
-use std::fs;
-
+struct Rectangle {
+width: f64,
+height: f64,
+}
+impl Rectangle {
+fn new(width: f64, height: f64) -> Rectangle {
+Rectangle { width, height }
+}
+fn area(&self) -> f64 {
+// TODO: implement
+self.width * self.height
+}
+fn perimeter(&self) -> f64 {
+// TODO: implement
+2.0 * (self.width + self.height)
+}
+fn is_square(&self) -> bool {
+// TODO: implement
+self.width == self.height
+}
+}
 fn main() {
-    if env::args().len() < 2 {
-        eprintln!("Program requires two arguments: <file path> <search name>");
-        std::process::exit(1);
-    }
-    let file_path = env::args().nth(1).unwrap();
-    let search_name = env::args().nth(2).unwrap();
-
-    for line in fs::read_to_string(file_path).unwrap().lines() {
-        if line == search_name {
-            println!("{} did walk on the Moon!", search_name);
-            return;
-        }
-    }
-
-    println!("{} did NOT walk on the Moon... YET!", search_name);
+let rect = Rectangle::new(10.0, 5.0);
+println!("Area: {}", rect.area());
+println!("Perimeter: {}", rect.perimeter());
+println!("Is square? {}", rect.is_square());
+assert!(Rectangle::new(5.0, 5.0).is_square());
+assert!(!Rectangle::new(5.0, 6.0).is_square());
 }
