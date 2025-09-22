40958317561f9a84f18b2da13ddd6fd5a0510b43
Chase <crebsto1@students.towson.edu>
2025-09-21 22:33:22 -0400

    while loop challenge

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
 fn main() {
-    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
-    let mut max: i32;
-    let mut min: i32;
-    let mut mean: f64;
+    let mut count = 0;
+    let letters = ['a', 'b', 'c'];
 
-    /* YOUR CODE GOES HERE */
-max = numbers[0];
-min = numbers[0];
-mean = 0.0;
-
-    for &num in numbers.iter() {
-        if num > max {
-            max = num;
-        }
-        else if num < min {
-            min = num;
-        }
-        mean += num as f64;
+    while count < letters.len() {
+        println!("letter is {}", letters[count]);
+        count += 1;
     }
-mean /= numbers.len() as f64;
-
-    assert_eq!(max, 56);
-    assert_eq!(min, -18);
-    assert_eq!(mean, 12.5);
-    println!("Tests passed!");
 }
