16b594f70d8332af9c024237b66011271a99e154
Chase <crebsto1@students.towson.edu>
2025-09-21 23:09:03 -0400

    lab4LastQuestion

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ struct Rectangle {
 width: f64,
 height: f64,
 }
+struct Circle {
+radius: f64, // assume radius > 0
+}
+
+impl Circle {
+    fn new(radius: f64) -> Circle {
+        Circle { radius }
+    }
+    fn area(&self) -> f64 {
+        let pi = 3.14;
+         pi * (self.radius * self.radius)
+    }
+    fn circumference(&self) -> f64 {
+        let pi = 3.14;
+        2.0 * pi * self.radius
+    }
+}
+
 impl Rectangle {
 fn new(width: f64, height: f64) -> Rectangle {
 Rectangle { width, height }
@@ -26,4 +44,8 @@ println!("Perimeter: {}", rect.perimeter());
 println!("Is square? {}", rect.is_square());
 assert!(Rectangle::new(5.0, 5.0).is_square());
 assert!(!Rectangle::new(5.0, 6.0).is_square());
+let c = Circle::new(5.0);
+println!("Area of circle: {}", c.area());
+println!("Circumference of circle: {}", c.circumference());
+
 }
