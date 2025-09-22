b155459657dc6626da60c2e220345811eefbe012
Chase <crebsto1@students.towson.edu>
2025-09-21 22:43:45 -0400

    checkroster ex

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-fn main() {
-    let mut count = 0;
-    let letters = ['a', 'b', 'c'];
+use std::env;
+use std::fs;
 
-    while count < letters.len() {
-        println!("letter is {}", letters[count]);
-        count += 1;
+fn main() {
+    if env::args().len() < 2 {
+        eprintln!("Program requires two arguments: <file path> <search name>");
+        std::process::exit(1);
     }
+    let file_path = env::args().nth(1).unwrap();
+    let search_name = env::args().nth(2).unwrap();
+
+    for line in fs::read_to_string(file_path).unwrap().lines() {
+        if line == search_name {
+            println!("{} did walk on the Moon!", search_name);
+            return;
+        }
+    }
+
+    println!("{} did NOT walk on the Moon... YET!", search_name);
 }
